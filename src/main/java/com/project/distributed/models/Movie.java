18f3acfde54c1fe49138c048f6d38a12b60fb454
Brian1011 <mutindabrian26@gmail.com>
2019-10-10 23:18:51 +0300

    add movie fields

--- src/main/java/com/project/distributed/models/Movie.java
+++ src/main/java/com/project/distributed/models/Movie.java
@@ -1,4 +1,13 @@
 package com.project.distributed.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "movies")
 public class Movie {
+    private long id;
+    private String name;
+    private long category_id;
+    private String type;
 }
