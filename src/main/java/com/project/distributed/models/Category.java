b0ed5458cbbe5f8a593b78708b3d8a4815b48fa3
Brian1011 <mutindabrian26@gmail.com>
2019-10-10 23:26:43 +0300

    add values to category model

--- src/main/java/com/project/distributed/models/Category.java
+++ src/main/java/com/project/distributed/models/Category.java
@@ -1,6 +1,48 @@
 package com.project.distributed.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private long id;
-    private String category;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    private Category(){
+
+    }
+
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", categoryName='" + categoryName + '\'' +
+                '}';
+    }
 }
