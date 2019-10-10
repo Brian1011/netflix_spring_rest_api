9cb2319d45f6a8495464279e13b76b266f2ba124
Brian1011 <mutindabrian26@gmail.com>
2019-10-10 23:30:48 +0300

    add values to client model

--- src/main/java/com/project/distributed/models/Client.java
+++ src/main/java/com/project/distributed/models/Client.java
@@ -1,7 +1,60 @@
 package com.project.distributed.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clients")
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private long id;
-    private String name;
+
+    @Column(name = "national_id")
     private long nationalId;
+
+    @Column(name = "name")
+    private String name;
+
+    public Client(){
+
+    }
+
+    public Client(long nationalId, String name) {
+        this.nationalId = nationalId;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(long nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", nationalId=" + nationalId +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
