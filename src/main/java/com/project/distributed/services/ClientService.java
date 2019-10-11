9f5d8b61e70f74da2c962a2bccd7c94b610ceeeb
Brian1011 <mutindabrian26@gmail.com>
2019-10-11 14:44:18 +0300

    add methods to service

--- src/main/java/com/project/distributed/services/ClientService.java
+++ src/main/java/com/project/distributed/services/ClientService.java
@@ -1,4 +1,15 @@
 package com.project.distributed.services;
 
+import com.project.distributed.models.Client;
+
+import java.util.List;
+
 public interface ClientService {
+    List<Client> findAll();
+
+    Client findById(long id);
+
+    Client createClient(Client client);
+
+    Client update(Client client);
 }
--- src/main/java/com/project/distributed/services/ClientServiceImpl.java
+++ src/main/java/com/project/distributed/services/ClientServiceImpl.java
@@ -1,4 +1,29 @@
 package com.project.distributed.services;
 
+import com.project.distributed.models.Client;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ClientServiceImpl implements ClientService {
+    @Override
+    public List<Client> findAll() {
+        return null;
+    }
+
+    @Override
+    public Client findById(long id) {
+        return null;
+    }
+
+    @Override
+    public Client createClient(Client client) {
+        return null;
+    }
+
+    @Override
+    public Client update(Client client) {
+        return null;
+    }
 }
